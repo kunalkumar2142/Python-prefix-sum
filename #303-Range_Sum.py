a17fd3651228f420e4cedcef4bf693268ea0809c
kunalkumar2142 <148185456+kunalkumar2142@users.noreply.github.com>
2025-01-11 22:27:28 +0530

    Create #303-Range_Sum.py

---
+++ #303-Range_Sum.py
@@ -0,0 +1,12 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefixSum = [0] * len(nums)
+        self.prefixSum[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.prefixSum[i] = self.prefixSum[i - 1] + nums[i]        
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.prefixSum[right]
+        return self.prefixSum[right] - self.prefixSum[left - 1]        

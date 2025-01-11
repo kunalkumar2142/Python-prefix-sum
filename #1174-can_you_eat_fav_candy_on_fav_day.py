2d79b3bb951ce9f0b8e15c6dcad1022750ae9105
kunalkumar2142 <148185456+kunalkumar2142@users.noreply.github.com>
2025-01-11 22:23:03 +0530

    Update #1174-can_you_eat_fav_candy_on_fav_day.py

--- #1174-can_you_eat_fav_candy_on_fav_day.py
+++ #1174-can_you_eat_fav_candy_on_fav_day.py
@@ -2,23 +2,17 @@ class Solution:
     def canEat(self, candiesCount: List[int], queries: List[List[int]]) -> List[bool]:
         n = len(candiesCount)
         answer = [False] * len(queries)
-        
         prefixSum = [0] * n
         prefixSum[0] = candiesCount[0]
         for i in range(1, n):
             prefixSum[i] = prefixSum[i - 1] + candiesCount[i]
-
         for i in range(len(queries)):
             favoriteType = queries[i][0]
             favoriteDay = queries[i][1]
             dailyCap = queries[i][2]
-            
             minCandiesNeeded = favoriteDay + 1
             maxCandiesEaten = (favoriteDay + 1) * dailyCap
-            
             candiesStart = 1 if favoriteType == 0 else prefixSum[favoriteType - 1] + 1
             candiesEnd = prefixSum[favoriteType]
-
             answer[i] = not (minCandiesNeeded > candiesEnd or maxCandiesEaten < candiesStart)
-
         return answer

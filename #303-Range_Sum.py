766f3fb8ef4f8fc017f21e481bf422bc1d1483f4
kunalkumar2142 <148185456+kunalkumar2142@users.noreply.github.com>
2025-01-11 23:20:49 +0530

    Update #303-Range_Sum.py

--- #303-Range_Sum.py
+++ #303-Range_Sum.py
@@ -1,10 +1,12 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
+        self.prefixSum = [0] * len(nums)
+        self.prefixSum[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.prefixSum[i] = self.prefixSum[i - 1] + nums[i]        
 
     def sumRange(self, left: int, right: int) -> int:
-        sum_ = 0 # sum is a keyword in Python, so using sum_
-        for i in range(left, right + 1):
-            sum_ += self.nums[i]
-        return sum_
+        if left == 0:
+            return self.prefixSum[right]
+        return self.prefixSum[right] - self.prefixSum[left - 1]        
